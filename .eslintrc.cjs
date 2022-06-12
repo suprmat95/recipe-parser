eea8afbd11ea028121dd8fdf90d49ba8fbe807e4
Ben White <benthewhite@gmail.com>
2022-06-12 15:32:19 +0200

    Removed comment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,4 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-
-  /*eslint no-control-regex: "error"*/
 };
